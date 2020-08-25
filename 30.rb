b635c7e3b53d4634a218cf4e92cfa3e6fdee15ec
TakahiroTanaka-dev <takahiro.forwork1130@gmail.com>
2020-08-25 23:53:06 +0900

    バイナリーサーチやり直し

--- 30.rb
+++ 30.rb
@@ -1,57 +1,25 @@
-def binary_search(array, number_of_elements, target)
+def binary_search(array, right, target)
   head = 0
-  while head <= number_of_elements
-    center = (head + number_of_elements) / 2
+  while head <= right
+    center = (head + right) / 2
     if array[center] == target
       return center
     elsif array[center] < target
-      head = center + 1
+      head = center +1
     else
-      number_of_elements = center -1
+      right = center -1
     end
   end
   return -1
 end
 
 array=[1,3,5,6,9,10,13,20,26,31]
-puts "検索したい数字を入力してください"
 target = gets.to_i
-number_of_elements = array.count
-
-result = binary_search(array, number_of_elements, target)
+elements_of_number = array.count
+result = binary_search(array, elements_of_number, target)
 
 if result == -1
   puts "#{target}は配列内に存在しません"
 else
   puts "#{target}は配列の#{result}番目に存在します "
 end
-
-
-# def binary_search(array, right, target)
-#   left = 0
-#   while left <= right
-#     center = (left + right) / 2
-#     if array[center] == target
-#       return center
-#     elsif array[center] < target
-#       left = center + 1
-#     else
-#       right = center - 1
-#     end
-#   end
-#   return -1 
-# end
-
-# array=[1,3,5,6,9,10,13,20,26,31]
-
-# puts "検索したい数字を入力してください"
-# target = gets.to_i
-# number_of_elements = array.count
-
-# result = binary_search(array, number_of_elements, target)
-
-# if result == -1
-#   puts "#{target}は配列内に存在しません"
-# else
-#   puts "#{target}は配列の#{result}番目に存在します "
-# end
