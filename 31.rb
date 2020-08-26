b7e2a1a8de855200903eeacc456fb8629c51657c
TakahiroTanaka-dev <takahiro.forwork1130@gmail.com>
2020-08-26 11:48:00 +0900

    模範回答を追加

--- 31.rb
+++ 31.rb
@@ -13,3 +13,15 @@ near_ten(12)
 near_ten(12)
 near_ten(17)
 near_ten(23)
+
+# 模範回答
+# def near_ten(num)
+#   remainder = num % 10
+#   if remainder <= 2 || remainder >= 8
+#     puts "True"
+#   elsif remainder <= 5
+#     puts "10の倍数との差は#{remainder}です"
+#   else 
+#     puts "10の倍数との差は#{10 - remainder}です"
+#   end
+# end
