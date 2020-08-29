766c554ded03aa05b8741bab63bf87d7c0a8f733
TakahiroTanaka-dev <takahiro.forwork1130@gmail.com>
2020-08-29 20:43:05 +0900

    absを使用したメソッド

---
+++ 34.rb
@@ -0,0 +1,31 @@
+def close_far(a, b, c)
+  x = (b-a).abs
+  y = (c-a).abs
+  z = (c-b).abs
+  if x == 1 && z >= 2
+    puts "True"
+  elsif y ==1 && z >= 2
+    puts "True"
+  else
+    puts "False"
+  end
+end
+
+close_far(1, 2, 10)
+close_far(1, 2, 3)
+close_far(4, 1, 3)
+
+
+# def close_number(a,b,c)
+#   x = (a-b).abs
+#   y = (a-c).abs
+#   z = (b-c).abs
+
+#   if x == 1 && z >= 2
+#     puts "True"
+#   elsif y == 1 && z >= 2
+#     puts "True"
+#   else
+#     puts "False"
+#   end
+# end
