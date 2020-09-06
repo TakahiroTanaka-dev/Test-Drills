2d7cffdd6e949111a19f2e242831f841f8b550e7
TakahiroTanaka-dev <takahiro.forwork1130@gmail.com>
2020-09-07 00:01:55 +0900

    金曜日とその他の曜日で出る文字を差別化

--- 4.rb
+++ 4.rb
@@ -1,5 +1,10 @@
 require "date"
 
 day = Date.today.wday
+days = ["日曜日", "月曜日", "火曜日", "水曜日", "木曜日", "金曜日", "土曜日"]
 
-puts day
+if day == 5
+  puts "今日は#{days[day]}曜日だ！！"
+else
+  puts "今日は#{days[day]}"
+end
