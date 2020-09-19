747dbacf1d1011033a0c1d2165ab2c56ea4bb374
TakahiroTanaka-dev <takahiro.forwork1130@gmail.com>
2020-09-20 02:39:42 +0900

    リファクタリング

--- 11.rb
+++ 11.rb
@@ -1,11 +1,20 @@
 def in1to10(num, outside_mode)
-  if 1 <= num <= 10
+  if 1 <= num and num <= 10
     puts "True"
-  elsif outside_mode == "true" && (num <= 0 || num >= 11)
+  elsif outside_mode === true && (num <= 0 || num >= 11)
     puts "True"
   else
     puts "False"
   end
 end
 
-in1to10(11, "true")
+in1to10(11, true)
+in1to10(11, false)
+
+# def in1to10(num, outside_mode)
+#   if (num >= 1 && num <= 10) || outside_mode
+#     puts "True"
+#   else
+#     puts "False"
+#   end
+# end
